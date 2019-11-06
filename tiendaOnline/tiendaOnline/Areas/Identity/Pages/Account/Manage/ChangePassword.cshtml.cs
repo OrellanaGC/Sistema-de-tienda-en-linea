2ed5f82dbabc3627bc7f8cb90f1f1c00a2b43bb8
lisjorge <daris10jorge@gmail.com>
2019-11-05 22:19:46 -0600

    traduccion de validaciones Perfil usuario, agregado de boton de vender aun sin funcionalidad

--- tiendaOnline/tiendaOnline/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ tiendaOnline/tiendaOnline/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -34,12 +34,12 @@ namespace tiendaOnline.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage ="EL campo de contraseña actual es requerido.")]
             [DataType(DataType.Password)]
             [Display(Name = "Current password")]
             public string OldPassword { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "EL campo de nueva contraseña es requerido.")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
