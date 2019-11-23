739aa40f67f5eba40efac3541132dbe8f51f8e70
OrellanaGC <og17007@ues.edu.sv>
2019-11-22 23:22:44 -0600

    restricciones en cupón

--- tiendaOnline/tiendaOnline/Models/Cupon.cs
+++ tiendaOnline/tiendaOnline/Models/Cupon.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using tiendaOnline.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace tiendaOnline.Models
 {
     public class Cupon
     {
         public int CuponID { get; set; }
+
+        [Display(Name = "Código del cupón")]
+        [Required(ErrorMessage = "Ingresar el Código del cupón"), MinLength(7, ErrorMessage = "Ingresar 7 caracteres"), StringLength(7, ErrorMessage = "Ingresar 7 caracteres")]
         public string codigoCupon { get; set; }
+
+        [Display(Name = "Monto del cupón")]
+        [Required(ErrorMessage = "Ingresar el monto del cupón"), Range(1.00, 20.00, ErrorMessage = "El cupón no puede valer más de $20.00")]
         public double montoCupon { get; set; }
         public Boolean estadoCupon { get; set; }
+
+        [Display(Name = "Fecha de creación del cupón")]
+        [Required(ErrorMessage = "Fecha de creación del cupón")]
         public DateTime fechaCreacion { get; set; }
+
+        [Display(Name = "Fecha de vencimiento del cupón")]
+        [Required(ErrorMessage = "Fecha de vencimiento del cupón")]
         public DateTime fechaVencimiento { get; set; }
+
+        [Display(Name = "Titulo del cupón")]
+        [Required(ErrorMessage = "Ingresar el Titulo del cupón"), MinLength(7, ErrorMessage = "Ingresar mínimo 7 caracteres"), StringLength(20, ErrorMessage = "Ingresar máximo 20 caracteres")]
         public string descripcionCupon { get; set; }
 
         //Relacion con usuarios
         public tiendaOnlineUser tiendaOnlineUser { get; set; }
         public string tiendaOnlineUserID { get; set; }
 
-        
+        public Cupon()
+        {
+            fechaCreacion = DateTime.Now;
+        }
 
 
     }
