a34ee7f34271c761c7326a4aee3ead1970ebeed6
Marhdz <marhdz0@gmail.com>
2019-11-23 17:18:40 -0600

    ApiKey nueva

--- tiendaOnline/tiendaOnline/Areas/Identity/Services/AuthMessageSenderOptions.cs
+++ tiendaOnline/tiendaOnline/Areas/Identity/Services/AuthMessageSenderOptions.cs
@@ -8,6 +8,6 @@ namespace tiendaOnline.Areas.Identity.Services
     public class AuthMessageSenderOptions
     {
         public string SendGridUser { get; set; }
-        public string SendGridKey = "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";
+        public string SendGridKey = "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";
     }
 }
